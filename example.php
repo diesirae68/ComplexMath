ca1fbaf6f4322e357032c5fc1258a3d022b507a5
VladB <diesirae68@gmail.com>
2024-04-28 03:40:42 +0300

    Replaced tabs

--- example.php
+++ example.php
@@ -8,28 +8,28 @@ use RB\Math\Complex;
 
 try {
 
-	// Two random complex numbers
-	$complex1 = new Complex(mt_rand(-1000, 1000), mt_rand(-1000, 1000));
-	$complex2 = new Complex(mt_rand(-1000, 1000), mt_rand(-1000, 1000));
+    // Two random complex numbers
+    $complex1 = new Complex(mt_rand(-1000, 1000), mt_rand(-1000, 1000));
+    $complex2 = new Complex(mt_rand(-1000, 1000), mt_rand(-1000, 1000));
 
-	// Addition
-	$complex = $complex1->add($complex2);
-	printf("%s + %s = %s;\n", $complex1, $complex2, $complex);
+    // Addition
+    $complex = $complex1->add($complex2);
+    printf("%s + %s = %s;\n", $complex1, $complex2, $complex);
 
-	// Subtraction
-	$complex = $complex1->sub($complex2);
-	printf("%s - %s = %s;\n", $complex1, $complex2, $complex);
+    // Subtraction
+    $complex = $complex1->sub($complex2);
+    printf("%s - %s = %s;\n", $complex1, $complex2, $complex);
 
-	// Multiplication
-	$complex = $complex1->mul($complex2);
-	printf("%s * %s = %s;\n", $complex1, $complex2, $complex);
+    // Multiplication
+    $complex = $complex1->mul($complex2);
+    printf("%s * %s = %s;\n", $complex1, $complex2, $complex);
 
-	// Division
-	$complex = $complex1->div($complex2);
-	printf("%s / %s = %s;\n", $complex1, $complex2, $complex);
+    // Division
+    $complex = $complex1->div($complex2);
+    printf("%s / %s = %s;\n", $complex1, $complex2, $complex);
 
 } catch (\Throwable $e) {
-	printf("Exception \"%s\" with message \"%s\" in %s on line %u", get_class($e), $e->getMessage(), $e->getFile(), $e->getLine());
+    printf("Exception \"%s\" with message \"%s\" in %s on line %u", get_class($e), $e->getMessage(), $e->getFile(), $e->getLine());
 } // end try
 
 ?>
--- src/Complex.php
+++ src/Complex.php
@@ -6,148 +6,148 @@ namespace RB\Math;
 
 final class Complex {
 
-	protected	$real,
-				$imag;
+    protected $real,
+              $imag;
 
-	public function __construct(float $real = NULL, float $imag = NULL) {
-		$this->real = $real ?? 0;
-		$this->imag = $imag ?? 0;
-	} // end of the '__construct()' constructor
+    public function __construct(float $real = NULL, float $imag = NULL) {
+        $this->real = $real ?? 0;
+        $this->imag = $imag ?? 0;
+    } // end of the '__construct()' constructor
 
-	public function __toString() : string {
-		return sprintf('complex(%s%si*%s)', $this->real, $this->imag < 0 ? '-' : '+', abs($this->imag));
-	} // end of the '__toString()' method
+    public function __toString() : string {
+        return sprintf('complex(%s%si*%s)', $this->real, $this->imag < 0 ? '-' : '+', abs($this->imag));
+    } // end of the '__toString()' method
 
-	public function __get(string $varname) : mixed {
-		switch ($varname) {
-			case 'real':
-			case 'imag':
-				return $this->$varname;
-			default:
-				throw new \Exception(sprintf('%s(): Unable to access the object property: \'%s::%s\'', __METHOD__, get_class($this), $varname));
-		} // end switch
-	} // end of the '__get()' method
+    public function __get(string $varname) : mixed {
+        switch ($varname) {
+            case 'real':
+            case 'imag':
+                return $this->$varname;
+            default:
+                throw new \Exception(sprintf('%s(): Unable to access the object property: \'%s::%s\'', __METHOD__, get_class($this), $varname));
+        } // end switch
+    } // end of the '__get()' method
 
-	public function __set(string $varname, mixed $value) : void {
-		switch ($varname) {
-			case 'real':
-			case 'imag':
-				$this->$varname = (float) $value;
-				break;
-			default:
-				throw new \Exception(sprintf('%s(): Unable to access the object property: \'%s::%s\'', __METHOD__, get_class($this), $varname));
-		} // end switch
-	} // end of the '__set()' method
+    public function __set(string $varname, mixed $value) : void {
+        switch ($varname) {
+            case 'real':
+            case 'imag':
+                $this->$varname = (float) $value;
+                break;
+            default:
+                throw new \Exception(sprintf('%s(): Unable to access the object property: \'%s::%s\'', __METHOD__, get_class($this), $varname));
+        } // end switch
+    } // end of the '__set()' method
 
-	public function add(self $y) : self {
-		$result = new self;
-		$result->real = $this->real + $y->real;
-		$result->imag = $this->imag + $y->imag;
-		return $result;
-	} // end of the 'add()' method
+    public function add(self $y) : self {
+        $result = new self;
+        $result->real = $this->real + $y->real;
+        $result->imag = $this->imag + $y->imag;
+        return $result;
+    } // end of the 'add()' method
 
-	public function sub(self $y) : self {
-		$result = new self;
-		$result->real = $this->real - $y->real;
-		$result->imag = $this->imag - $y->imag;
-		return $result;
-	} // end of the 'sub()' method
+    public function sub(self $y) : self {
+        $result = new self;
+        $result->real = $this->real - $y->real;
+        $result->imag = $this->imag - $y->imag;
+        return $result;
+    } // end of the 'sub()' method
 
-	public function mul(self $y) : self {
-		$result = new self;
-		$result->real = $this->real * $y->real - $this->imag * $y->imag;
-		$result->imag = $this->imag * $y->real + $this->real * $y->imag;
-		return $result;
-	} // end of the 'mul()' method
+    public function mul(self $y) : self {
+        $result = new self;
+        $result->real = $this->real * $y->real - $this->imag * $y->imag;
+        $result->imag = $this->imag * $y->real + $this->real * $y->imag;
+        return $result;
+    } // end of the 'mul()' method
 
-	public function div(self $y) : self {
-		$result = new self;
-		$tmp = $this->real * $this->real + $this->imag * $this->imag;
-		if ($tmp == 0.0) {
-			throw new \Exception(sprintf('%s(): Division by zero', __METHOD__));
-		} // end if
-		$result->real = ($this->real * $y->real + $this->imag * $y->imag) / $tmp;
-		$result->imag = ($this->real * $y->imag - $this->imag * $y->real) / $tmp;
-		return $result;
-	} // end of the 'div()' method
+    public function div(self $y) : self {
+        $result = new self;
+        $tmp = $this->real * $this->real + $this->imag * $this->imag;
+        if ($tmp == 0.0) {
+            throw new \Exception(sprintf('%s(): Division by zero', __METHOD__));
+        } // end if
+        $result->real = ($this->real * $y->real + $this->imag * $y->imag) / $tmp;
+        $result->imag = ($this->real * $y->imag - $this->imag * $y->real) / $tmp;
+        return $result;
+    } // end of the 'div()' method
 
-	public function pow(int $n) : self {
-		$n = abs($n);
-		$result = new self;
-		$pow_mod = pow($this->abs(), $n);
-		$phi_mul_n = $this->phi() * $n;
-		$result->real = $pow_mod * cos($phi_mul_n);
-		$result->imag = $pow_mod * sin($phi_mul_n);
-		return $result;
-	} // end of the 'pow' method
+    public function pow(int $n) : self {
+        $n = abs($n);
+        $result = new self;
+        $pow_mod = pow($this->abs(), $n);
+        $phi_mul_n = $this->phi() * $n;
+        $result->real = $pow_mod * cos($phi_mul_n);
+        $result->imag = $pow_mod * sin($phi_mul_n);
+        return $result;
+    } // end of the 'pow' method
 
-	public function sqrt() : self {
-		$result = new self;
-		if (!empty($this->real) || !empty($this->imag)) {
-			$real = abs($this->real);
-			$imag = abs($this->imag);
-			if (empty($real)) {
-				$abs = $imag;
-			} elseif (empty($imag)) {
-				$abs = $real;
-			} else {
-				$abs = sqrt($real * $real + $imag * $imag);
-			} // end if
-			$result->real = sqrt(($abs + $this->real) * 0.5);
-			$result->imag = sqrt(($abs - $this->real) * 0.5);
-			if ($this->imag < 0.0) {
-				$result->imag = -$result->imag;
-			} // end if
-		} // end if
-		return $result;
-	} // end of the 'sqrt()' method
+    public function sqrt() : self {
+        $result = new self;
+        if (!empty($this->real) || !empty($this->imag)) {
+            $real = abs($this->real);
+            $imag = abs($this->imag);
+            if (empty($real)) {
+                $abs = $imag;
+            } elseif (empty($imag)) {
+                $abs = $real;
+            } else {
+                $abs = sqrt($real * $real + $imag * $imag);
+            } // end if
+            $result->real = sqrt(($abs + $this->real) * 0.5);
+            $result->imag = sqrt(($abs - $this->real) * 0.5);
+            if ($this->imag < 0.0) {
+                $result->imag = -$result->imag;
+            } // end if
+        } // end if
+        return $result;
+    } // end of the 'sqrt()' method
 
-	public function exp() : self {
-		$result = new self;
-		$er = exp($this->real);
-		$result->real = $er * cos($this->imag);
-		$result->imag = $er * sin($this->imag);
-		return $result;
-	} // end of the 'exp()' method
+    public function exp() : self {
+        $result = new self;
+        $er = exp($this->real);
+        $result->real = $er * cos($this->imag);
+        $result->imag = $er * sin($this->imag);
+        return $result;
+    } // end of the 'exp()' method
 
-	public function log(int $k = NULL) : self {
-		$k = $k ?? 0;
-		$mod = $this->abs();
-		if ($mod == 0.0) {
-			throw new \Exception(sprintf('%s(): The modulus of a complex number must be non-zero', __METHOD__));
-		} // end if
-		$result = new self;
-		$result->real = log($mod);
-		$result->imag = $this->phi() + 2 * M_PI * $k;
-		return $result;
-	} // end of the 'log()' method
+    public function log(int $k = NULL) : self {
+        $k = $k ?? 0;
+        $mod = $this->abs();
+        if ($mod == 0.0) {
+            throw new \Exception(sprintf('%s(): The modulus of a complex number must be non-zero', __METHOD__));
+        } // end if
+        $result = new self;
+        $result->real = log($mod);
+        $result->imag = $this->phi() + 2 * M_PI * $k;
+        return $result;
+    } // end of the 'log()' method
 
-	public function inv() : self {
-		$result = new self;
-		$result->real =  $this->real;
-		$result->imag = -$this->imag;
-		return $result;
-	} // end of the 'inv()' method
+    public function inv() : self {
+        $result = new self;
+        $result->real =  $this->real;
+        $result->imag = -$this->imag;
+        return $result;
+    } // end of the 'inv()' method
 
-	public function abs() : self {
-		if (!empty($this->real) || !empty($this->imag)) {
-			$real = abs($this->real);
-			$imag = abs($this->imag);
-			if (empty($real)) {
-				$abs = $imag;
-			} elseif (empty($imag)) {
-				$abs = $real;
-			} else {
-				$abs = sqrt($real * $real + $imag * $imag);
-			} // end if
-			return $abs;
-		} // end if
-		return 0;
-	} // end of the 'abs()' method
+    public function abs() : self {
+        if (!empty($this->real) || !empty($this->imag)) {
+            $real = abs($this->real);
+            $imag = abs($this->imag);
+            if (empty($real)) {
+                $abs = $imag;
+            } elseif (empty($imag)) {
+                $abs = $real;
+            } else {
+                $abs = sqrt($real * $real + $imag * $imag);
+            } // end if
+            return $abs;
+        } // end if
+        return 0;
+    } // end of the 'abs()' method
 
-	public function phi() : float {
-		return atan2($this->imag, $this->real);
-	} // end of the 'phi()' method
+    public function phi() : float {
+        return atan2($this->imag, $this->real);
+    } // end of the 'phi()' method
 
 } // end of the 'Complex' class
 
